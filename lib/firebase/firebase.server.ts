9a89387481691fddf21e498073735644e21fe6db
wojtek <wojciech.podlesny1@gmail.com>
2025-08-12 21:51:50 +0200

    Update files firebase

--- lib/firebase/firebase.server.ts
+++ lib/firebase/firebase.server.ts
@@ -1,9 +1,9 @@
 import { cert, getApps, initializeApp, App, ServiceAccount } from "firebase-admin/app";
 
 const serviceAccount: ServiceAccount = {
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-  privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, "\n"),
+  project_id: process.env.FIREBASE_PROJECT_ID,
+  client_email: process.env.FIREBASE_CLIENT_EMAIL,
+  private_key: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, "\n"),
 };
 
 export function getAdminApp(): App {
