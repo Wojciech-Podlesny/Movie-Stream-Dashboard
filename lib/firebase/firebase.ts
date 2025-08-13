6db6b0ce79a8758e4f93b4eb857f993f6f36eaf5
wojtek <wojciech.podlesny1@gmail.com>
2025-08-13 22:08:25 +0200

    Update projects

--- lib/firebase/firebase.ts
+++ lib/firebase/firebase.ts
@@ -2,15 +2,6 @@ import { initializeApp, getApps, getApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
-// const firebaseConfig = {
-//   apiKey: process.env.API_KEY_FIREBASE!,
-//   authDomain: process.env.AUTH_DOMAIN_FIREBASE!,
-//   projectId: process.env.PROJECT_ID_FIREBASE!,
-//   storageBucket: process.env.STORAGE_BUCKET_FIREBASE!,
-//   messagingSenderId: process.env.MESSAGING_SENDER_ID_FIREBASE!,
-//   appId: process.env.APP_ID_FIREBASE!,
-// };
-
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_API_KEY_FIREBASE!,
   authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN_FIREBASE!,
